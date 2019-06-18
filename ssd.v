32102b27d017643043bc01867eb8171a0b2f4bd5
mkorzeniowska <51078508+mkorzeniowska@users.noreply.github.com>
2019-06-18 11:25:36 +0200

    Add files via upload

---
+++ ssd.v
@@ -0,0 +1,152 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.06.2019 11:17:32
+// Design Name: 
+// Module Name: ssd
+// Project Name: 
+// Target Devices: 
+// Tool Versions:
+// Description: This module drives the seven segment displays. It has got 4 different inputs
+//						for the 4 digits on the board. It's inputs are not directly the binary numbers.
+//						You need to convert 4-bit hex values to their seven segment display equivalents.
+//						 
+//						a0,b0,c0,d0,e0,f0,g0 belongs to digit0, rigth most digit on the board.
+//						a1,b1,c1,d1,e1,f1,g1 belongs to digit1, 2. from the rigth digit on the board.
+//						a2,b2,c2,d2,e2,f2,g2 belongs to digit2, 3. from the rigth digit on the board.
+//						a3,b3,c3,d3,e3,f3,g3 belongs to digit3, left most digit on the board.	
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+///////////////////////////////////////////////////////////////////////////////////
+
+
+module ssd( clk, reset, a0, a1, a2, a3, b0, b1, b2, b3, c0, c1, c2, c3, d0, d1, d2, d3, e0, e1, e2, e3, 
+       f0, f1, f2, f3, g0, g1, g2, g3, a, b, c, d, e, f, g, an0, an1, an2, an3
+    );
+    
+    input clk, reset;            //set clock and reset as input(1 bit)
+    input a0, a1, a2, a3, b0, b1, b2, b3, c0, c1, c2, c3, d0, d1, d2, d3, e0, e1, e2, e3,  //set display data as input (1 bit)
+           f0, f1, f2, f3, g0, g1, g2, g3;
+    output reg a, b, c, d, e, f, g, an0, an1, an2, an3;  // set outputs as reg (1 bit)
+    
+    reg [2:0] state;  //hold state number (3 bit)
+    reg [14:0] counter;  ////counter to slow the input clock(15 bit)
+    
+    //in this always block the speed of the clock reduced by 25000 times so that display works properly
+    always @(posedge clk) begin            //state counter
+            if (reset) begin              //synchronus reset 
+                     state <= 0;         //if reset set state and counter to zero
+                     counter <= 0;
+            end else begin              //else the counter untill 25000
+                    if (counter == 15'h61A8) begin           //if equal to 25000
+                             if (state == 3'b100)            //if it is the last state return to state 1
+                             state <= 1;
+                             else                          // go on state up
+                             state <= state + 1;
+                    counter <= 0;                       //0 the counter because after it reaches 25000
+                    end
+                    else 
+                    counter <= counter + 1;          //if not 25000 add 1
+            end
+     end
+     
+    //in this always block we give the inputs to the leds by choosing 
+    //different display segment in each time
+    //!(In the button-pin sheet of the Basys, the 7-SEG numbers are assinged wrong.
+    //To avoid confusion we corrected it while choosing an0,an1... So if you just enter
+    //pin numbers according to the sheet it will work fine.)
+    always @(posedge clk) 
+    begin
+          if (reset)       //if reset initilize the outputs
+          begin
+          an0 <= 1;
+          an1 <= 1;
+          an2 <= 1;
+          an3 <= 1;
+          a <= 1;
+          b <= 1;
+          c <= 1;
+          d <= 1;
+          e <= 1;
+          f <= 1;
+          g <= 1;
+          end
+          else if (state == 3'b001)   //state 1 gives the led outputs to the AN0
+          begin
+          an0 <= 1;
+          an1 <= 1;
+          an2 <= 1;
+          an3 <= 1;
+          a <= 0;
+          b <= 0;
+          c <= 0;
+          d <= 0;
+          e <= 0;
+          f <= 0;
+          g <= 0;
+          end
+          else if (state == 3'b010)    //state 2 gives the led outputs to the AN1
+          begin
+          an0 <= 0;
+          an1 <= 1;
+          an2 <= 1;
+          an3 <= 1;
+          a <= a1;
+          b <= b1;
+          c <= c1;
+          d <= d1;
+          e <= e1;
+          f <= f1;
+          g <= g1;
+          end
+          else if (state == 3'b011)   //state 3 gives the led outputs to the AN2
+          begin
+          an0 <= 1;
+          an1 <= 0;
+          an2 <= 1;
+          an3 <= 1;
+          a <= a2;
+          b <= b2;
+          c <= c2;
+          d <= d2;
+          e <= e2;
+          f <= f2;
+          g <= g2;
+          end
+          else if (state == 3'b100)    //state 4 gives the led outputs to the AN3
+          begin
+          an0 <= 1;
+          an1 <= 1;
+          an2 <= 0;
+          an3 <= 1;
+          a <= a3;
+          b <= b3;
+          c <= c3;
+          d <= d3;
+          e <= e3;
+          f <= f3;
+          g <= g3;
+          end
+          else   //for other states default inputs and outputs
+          begin
+          an0 <= 1;
+          an1 <= 1;
+          an2 <= 1;
+          an3 <= 1;
+          a <= 1;
+          b <= 1;
+          c <= 1;
+          d <= 1;
+          e <= 1;
+          f <= 1;
+          g <= 1;
+          end
+    end    
+endmodule
---
+++ vending_machine.v
@@ -0,0 +1,264 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.06.2019 21:05:15
+// Design Name: 
+// Module Name: vending_machine
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vending_machine(clk, reset, BTN1, BTN2, BTN3, Money_in, product1, product2, product3, delivered, 
+                       LED1, LED2, LED3, digit0, digit1, digit2, digit3
+                       );
+input clk;     //internal clock (50MHz)
+input reset;   //reset variable
+input BTN1, BTN2, BTN3;    
+input Money_in;
+//BTN1: product1 - tea, BTN2: product2 - coffee, BTN3: product3 - hot chocolate
+//Price: tea: 2z³, coffee: 3z³, hot chocolate: 5z³
+
+output reg product1, product2, product3, delivered;
+output reg [6:0] digit0; 
+output reg [6:0] digit1;
+output reg [6:0] digit2;
+output reg [6:0] digit3;
+
+output LED1, LED2, LED3;   //LEDs showing which product was selected
+
+assign LED1 = BTN1;
+assign LED2 = BTN2;
+assign LED3 = BTN3;
+
+reg [7:0] insertedMoney;
+reg [7:0] totalChange;
+reg [7:0] change;
+
+reg [1:0] current_state, next_state;
+
+parameter free_state = 0, initial_state=1,tea_0=2, coffee_0=3, hot_chocolate_0=4, delivered_state=5;
+   
+//*****************************************************************************
+
+always @(posedge clk or posedge reset)
+begin
+    if (reset==1) begin
+       current_state <= initial_state;
+       insertedMoney = 8'b00000000;
+       totalChange = 8'b00000000;
+       //money = 8'b0000000;
+       change <= 8'b00000000;
+       digit0<=7'b1111110;
+       digit1<=7'b1111110;
+       digit2<=7'b1111110;
+       digit3<=7'b1111110;
+       product1<=0;
+       product2<=0;
+       product3<=0;
+       delivered<=0;    
+    end
+    else begin
+       if (current_state != initial_state)
+       begin
+           if (Money_in == 1 ) insertedMoney = 8'b00000001;
+           else if (Money_in == 2) insertedMoney = 8'b00000010;
+           else if (Money_in == 3) insertedMoney = 8'b00000011;
+           else if (Money_in == 4) insertedMoney = 8'b00000100;
+           else if (Money_in == 5) insertedMoney = 8'b00000101;
+           else if (Money_in == 6) insertedMoney = 8'b00000110;
+           else if (Money_in == 7) insertedMoney = 8'b00000111;
+       end
+       current_state <= next_state;
+       totalChange <= totalChange + change;
+end
+
+//**********************************************************************8******
+if (current_state != delivered_state)     //display inserted money
+begin
+    if (insertedMoney == 8'b00000000)
+    begin
+    digit0 = 7'b0000001;   //0
+    digit1 = 7'b0000001;
+    end
+    else if (insertedMoney == 8'b00000001)  //1
+    begin
+    digit0 = 7'b1001111;
+    digit1 = 7'b1111110;             // -
+    end
+    else if (insertedMoney == 8'b00000010)    //2
+    begin
+    digit0 = 7'b0010010;
+    digit1 = 7'b0000001;
+    end
+    else if (insertedMoney == 8'b00000011)    //3
+    begin
+    digit0 = 7'b0000110;
+    digit1 = 7'b1111110;
+    end
+    else if (insertedMoney == 8'b00000100)    //4
+    begin
+    digit0 = 7'b1001100;
+    digit1 = 7'b1111110;
+    end
+    else if (insertedMoney == 8'b00000101)  //5
+    begin
+    digit0 = 7'b0100100;
+    digit1 = 7'b1111110;
+    end
+    else if (insertedMoney == 8'b00000110)   //6
+    begin
+    digit0 = 7'b1011111;
+    digit1 = 7'b1111110;
+    end
+    else if (insertedMoney == 8'b00000111)   //7
+    begin
+    digit0 = 7'b0001111;
+    digit1 = 7'b1111110;
+    end
+end
+else begin                        //money refunded
+   digit0 = 7'b1111110;
+   digit1 = 7'b1111110;
+   if (totalChange == 8'b00000000)
+      begin
+      digit2 = 7'b0000001;
+      digit3 = 7'b0000001;
+      end
+   else if (totalChange == 8'b00000001)  //1
+      begin
+      digit2 = 7'b1111110;    // -
+      digit3 = 7'b1001111;    // 1
+      end
+   else if (totalChange == 8'b00000010)   //2
+      begin
+      digit2 = 7'b1111110;
+      digit3 = 7'b0010010;
+      end
+   else if (totalChange == 8'b00000011)   //3
+      begin
+      digit2 = 7'b1111110;
+      digit3 = 7'b0000110;
+      end
+   else if (totalChange == 8'b00000100)   //4
+      begin
+      digit2 = 7'b1111110;
+      digit3 = 7'b1001100;
+      end
+   else if (totalChange == 8'b00000101)  //5
+      begin
+      digit2 = 7'b1111110;
+      digit3 = 7'b0100100;
+      end
+   end
+end
+//***************************************************************************
+// next state and output combinational logics
+//****************************************************************************
+always @*
+begin
+case (current_state)
+initial_state: begin
+              delivered = 0;
+              change = 8'b000000000;
+              if (BTN1 ==1 ) begin
+                  product1 = 1;
+                  product2 = 0;
+                  product3 = 0;
+                  next_state = tea_0;
+                  end
+              else if (BTN2 == 1) begin
+                  product1 = 0;
+                  product2 = 1;
+                  product3 = 0;
+                  next_state = coffee_0;
+                  end
+              else if (BTN3 == 1) begin
+                  product1 = 0;
+                  product2 = 0;
+                  product3 = 1;
+                  next_state <= hot_chocolate_0;
+                  end
+              else begin
+                  next_state <= initial_state;
+                  end
+              end    
+              
+ 
+tea_0: begin
+       if (insertedMoney >= 2 ) begin
+           product1 = 1;
+           product2 = 0;
+           product3 = 0;
+           //delivered = 1;
+           change = insertedMoney - 2;
+           next_state = delivered_state;
+           end
+       else begin
+           product1 = 1;
+           product2 = 0;
+           product3 = 0;
+           delivered = 0;
+           change = 8'b00000000;
+           next_state = tea_0;
+           end
+       end
+       
+coffee_0: begin
+       if (insertedMoney >= 3 ) begin
+           product1 = 0;
+           product2 = 1;
+           product3 = 0;
+           //delivered = 1;
+           change = insertedMoney - 3;
+           next_state = delivered_state;
+           end
+       else begin
+           product1 = 0;
+           product2 = 1;
+           product3 = 0;
+           delivered = 0;
+           change = 8'b00000000;
+           next_state = coffee_0;
+           end
+       end
+hot_chocolate_0: begin
+          if (insertedMoney >= 5 ) begin
+             product1 = 0;
+             product2 = 0;
+             product3 = 1;
+             //delivered = 1;
+             change = insertedMoney - 5;
+             next_state = delivered_state;
+             end
+          else begin
+             product1 = 0;
+             product2 = 0;
+             product3 = 1;
+             delivered = 0;
+             change = 8'b00000000;
+             next_state = hot_chocolate_0;
+             end
+         end       
+         
+delivered_state: begin
+          next_state = delivered_state;
+          change = 8'b00000000;
+          delivered = 1;
+          end
+endcase
+end
+  
+
+endmodule
